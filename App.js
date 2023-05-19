78d417e04d4e18bd168f07c4591d2e0591b80896
RenatoApache <renatodtsato@gmail.com>
2023-05-19 01:09:06 +0000

    finalizando o app de calcular imc

--- App.js
+++ App.js
@@ -1,7 +1,35 @@
-import React from 'react';//<--- mudei aqui
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+	const [peso, setPeso] = useState(0);
+	const [altura, setAltura] = useState(0);
+	const [resultado, setResultado] = useState(0);
+	const [imc, setImc] = useState("");
+	
+	function Calcular (){
+		let r = parseFloat(peso) / (parseFloat(altura)*parseFloat(altura));
+		if (r<=16.9){
+			setImc("- Muito abaixo do peso");
+		}else if(r >= 17 && r <= 24.9){
+			setImc("- Peso normal");
+		}else if(r >= 25 && r <=29.9){
+			setImc("- Acima do peso");
+		}else if(r >= 30 && r <=34.9){
+			setImc("- Obesidade grau I");
+		}else if(r >= 35 && r <= 40){
+			setImc("- Obesidade grau II");
+		}else{
+			setImc("- Obesidade grau III");
+		}
+		setResultado("imc = "+r.toFixed(2));
+	}
+	function Limpar (){
+		setPeso(0);
+		setAltura(0);
+		setResultado(0);
+		setImc("");
+	}
   return (
     <View style={styles.container}>
 		<View style={styles.titulo}>
@@ -19,23 +47,31 @@ export default function App() {
 			<Text style={styles.label}>Peso: </Text>
 			<TextInput 
 				style={styles.input}	
+				value={peso}
+				onChangeText={(peso)=>setPeso(peso)}
 			/>
 			
 			<Text style={styles.label}>Altura: </Text>
 			<TextInput 
 				style={styles.input}	
+				value={altura}
+				onChangeText={(altura)=>setAltura(altura)}
 			/>
 		</View>
 		<View style={styles.blocoBotao}>
-			<TouchableOpacity style={styles.botao}>
+			<TouchableOpacity style={styles.botao} onPress={Calcular}>
 				<Text style={styles.textoBotao}>Calcular</Text>
 			</TouchableOpacity>
 			
-			<TouchableOpacity style={styles.botao}>
+			<TouchableOpacity style={styles.botao} onPress={Limpar}>
 				<Text style={styles.textoBotao}>Limpar</Text>
 			</TouchableOpacity>
 		</View>
      
+		   <View style={styles.boxResul}>
+			  <Text style={styles.resul}> Resultado: {resultado} {imc}</Text>
+		  </View>
+		  
     </View>	
   );
 }
@@ -46,7 +82,7 @@ const styles = StyleSheet.create({
 	backgroundColor:'#fff',
   },
   titulo:{
-	  backgroundColor:'#ffa500',
+	  backgroundColor:'#8ba3b1',
 	  height:'10%',
 	  justifyContent:'center'
   },
@@ -81,7 +117,7 @@ const styles = StyleSheet.create({
 	  marginLeft:'10%'
   },
   botao:{
-	  backgroundColor:'#ffa500',
+	  backgroundColor:'#8ba3b1',
 	  width:'40%',
 	  height:40,
 	  justifyContent:'center',
@@ -91,6 +127,14 @@ const styles = StyleSheet.create({
   },
   textoBotao:{
 	  textAlign:'center',
-	  fontSize:20
-  }
+	  fontSize:20,
+	  color:'#fff'
+  },
+	boxResul:{
+		textAlign:'center',
+		marginTop:100,
+	},
+	resul:{
+		fontSize:30,
+	}
 });
